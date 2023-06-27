f97a0dd2a776231fb8ebcb8451b3c73856c712d4
thibaud75 <thibaud.delvert@gmail.com>
2023-06-27 15:47:19 +0200

    add inscription.tsx + router inscripiton

--- src/Router/MainRouter.tsx
+++ src/Router/MainRouter.tsx
@@ -1,17 +1,18 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "../pages/Auth";
-import App from "../App"
-
+import Inscription from "../pages/Inscription";
+import App from "../App";
 
 export default function MainRouter() {
   return (
-     <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<App />}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
         <Route path="/auth" element={<Auth />} />
-    </Routes>
-</BrowserRouter> 
+        <Route path="/signup" element={<Inscription />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Inscription.tsx
+++ src/pages/Inscription.tsx
@@ -1,62 +1,68 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import './App.css'
-import Nav from '../components/Nav'
-import Footer from '../components/Footer'
-
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 function Auth() {
-  
-  const getData = (e:React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    
+  const getData = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
     const formData = new FormData(e.currentTarget);
-    
-    const email = formData.get('email')
-    let newEmail = email as string
-  
-    const password = formData.get('password')
-    let newPassword = password as string
-  
-  
+
+    const email = formData.get("email");
+    let newEmail = email as string;
+
+    const password = formData.get("password");
+    let newPassword = password as string;
+
+    const name = formData.get("name");
+    let newName = name as string;
+
+    const lastname = formData.get("lastname");
+    let newLastname = lastname as string;
+
     const data = {
-      email : newEmail,
-      password : newPassword,
-    }
-  
-    callApi(data)
-  }
-  
-  function callApi(login:{}){
+      email: newEmail,
+      password: newPassword,
+      name: newName,
+      lastname: newLastname,
+    };
+
+    callApi(data);
+  };
+
+  function callApi(login: {}) {
     fetch(`http://localhost:3000/auth/signup`, {
-      method : 'POST',
-      headers :{
-        'Content-Type': 'application/json',
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(login),
     })
-  
-    .then(response => {
-      console.log(response)
-      return response.json()
-    })
-    .then(data => {
-      console.log(data)
-    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
   }
   return (
     <>
-      <Nav/>
+      <Nav />
       <div>
-        <h1 className='inscription'>Inscrpition</h1>
-        <form onSubmit={(e)=> getData(e)}>
-          <input type = 'text' placeholder='email' name='email'></input>
-          <input type = 'text' placeholder='mdp' name='password'></input>
-          <button type='submit'></button>
+        <h1 className="inscription">Inscrpition</h1>
+        <form onSubmit={(e) => getData(e)}>
+          <input type="text" placeholder="email" name="email"></input>
+          <input type="text" placeholder="mdp" name="password"></input>
+          <input type="text" placeholder="name" name="name"></input>
+          <input type="text" placeholder="lastname" name="lastname"></input>
+          <button type="submit"></button>
         </form>
       </div>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Auth
+export default Auth;
