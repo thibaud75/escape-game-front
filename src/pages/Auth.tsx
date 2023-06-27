44b57ed8ffe618f8944a7419155edc4dec667e5b
Marc-E-B <marc.buran@outlook.fr>
2023-06-27 15:37:45 +0200

    Add npm react-icons / Add Auth.css

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,62 +1,61 @@
-import { useState } from 'react'
-// import './App.css'
-import Nav from '../components/Nav'
-import Footer from '../components/Footer'
-
+import { useState } from "react";
+import "./Auth.css";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import { HiOutlineLogin } from "react-icons/hi";
 
 function Auth() {
-  
-  const getData = (e:React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    
+  const getData = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
     const formData = new FormData(e.currentTarget);
-    
-    const email = formData.get('email')
-    let newEmail = email as string
-  
-    const password = formData.get('password')
-    let newPassword = password as string
-  
-  
+
+    const email = formData.get("email");
+    let newEmail = email as string;
+
+    const password = formData.get("password");
+    let newPassword = password as string;
+
     const data = {
-      email : newEmail,
-      password : newPassword,
-    }
-  
-    callApi(data)
-  }
-  
-  function callApi(login:{}){
+      email: newEmail,
+      password: newPassword,
+    };
+
+    callApi(data);
+  };
+
+  function callApi(login: {}) {
     fetch(`http://localhost:3000/auth/login`, {
-      method : 'POST',
-      headers :{
-        'Content-Type': 'application/json',
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(login),
     })
-  
-    .then(response => {
-      console.log(response)
-      return response.json()
-    })
-    .then(data => {
-      console.log(data)
-    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
   }
   return (
     <>
-      <Nav/>
+      <Nav />
       <div>
-        <h1 className='connexion'>connexion</h1>
-        <form onSubmit={(e)=> getData(e)}>
-          <input type = 'text' placeholder='email' name='email'></input>
-          <input type = 'text' placeholder='mdp' name='password'></input>
-          <button type='submit'></button>
+        <h1 className="connexion">
+          connexion <HiOutlineLogin />
+        </h1>
+        <form onSubmit={(e) => getData(e)}>
+          <input type="text" placeholder="email" name="email"></input>
+          <input type="text" placeholder="mdp" name="password"></input>
+          <button type="submit">Submit</button>
         </form>
       </div>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Auth
+export default Auth;
