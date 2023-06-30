a4996827c3ae299ef0423116d8654198e4c137ec
Mike Firpionn <mike.firpionn75@gmail.com>
2023-06-30 14:13:30 +0200

    fonctionnel

--- src/pages/SuccesOrder.tsx
+++ src/pages/SuccesOrder.tsx
@@ -1,15 +1,54 @@
-import Nav from "../components/Nav"
-import Footer from "../components/Footer"
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
-function SuccesOrder(){
+function SuccesOrder() {
+  const [date, setDate] = useState();
+  const [gameId, setGameId] = useState();
+  const [gameName, setGameName] = useState();
 
-    return(
+  function apiForDate() {
+    fetch("http://localhost:3000/disponibility/getalldispo")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[data.length - 1]);
+        // console.log(data[0].dispo.disponibility[0].date)
+
+        setDate(data[data.length - 1].disponibility[0].date);
+        setGameId(data[data.length - 1].gameId);
+      });
+      
+  }
+
+  const user = localStorage.getItem("userName");
+
+function apiForNameGame() {
+    fetch(`http://localhost:3000/games/${gameId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(gameId)
+        console.log(data.name);
+        setGameName(data.name);
+      })
+  }
+
+  useEffect(() => {
+    apiForDate();
+  }, []);
+
+  useEffect(() =>{
+    apiForNameGame()
+  },[gameId])
+
+  return (
     <>
-    <Nav/>
-        <p>Vous avez bien reservé</p>
-    <Footer/>
+      <Nav />
+
+      <p>Félicitation {user}, notre equipe vous attend le {date} dans notre salle {gameName} pour l'experience la plus démentielle de votre vie ! ps : N'oubliez pas de laissez votre peur à la maison  </p>
+      <Footer />
     </>
-    )
+  );
 }
 
-export default SuccesOrder
+export default SuccesOrder;
