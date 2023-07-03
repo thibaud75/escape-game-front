68cd6cfcfe19fee032efad4b78247ad278e4274e
thibaud75 <thibaud.delvert@gmail.com>
2023-07-03 17:23:40 +0200

    sans stress

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -23,6 +23,7 @@ interface ReservHistory {
 
 const History = () => {
   const [reservHistory, setReservHistory] = useState<ReservHistory[]>([]);
+  const [arrayTimestamp, setArrayTimestamp] = useState<number[]>([]);
 
   useEffect(() => {
     fetch(
@@ -53,6 +54,59 @@ const History = () => {
       return <h2 className="h2resa">Réservations de {userName}</h2>;
     }
   }
+
+  const getMonthNumber = (monthName: string) => {
+    const date = new Date(`${monthName} 1, 2000`);
+    return date.toLocaleString("fr-FR", { month: "numeric" });
+  };
+
+  const getTimestamp = () => {
+    const dateString: string[] = [];
+
+    reservHistory.forEach((element, index) => {
+      dateString.push(element.disponibility[0].date);
+
+      // Convertir le jour de la semaine en numéro (0 pour dimanche, 1 pour lundi, etc.)
+      const dayOfWeek = [
+        "Dimanche",
+        "Lundi",
+        "Mardi",
+        "Mercredi",
+        "Jeudi",
+        "Vendredi",
+        "Samedi",
+      ].indexOf(dateString[index].split(" ")[0]);
+
+      // Extraire le jour et le mois de la date
+      const day = dateString[index].split(" ")[1];
+      const month = getMonthNumber(dateString[index].split(" ")[2]);
+      console.log(month);
+
+      // Extraire l'année de la date
+      const year = dateString[index].split(" ")[3];
+
+      // Déterminer l'heure en fonction de la partie "matin" ou "après-midi"
+      const time = dateString[index].includes("matin")
+        ? "10:00:00"
+        : "14:00:00";
+
+      // console.log(year, day, time);
+      // Construire la date au format ISO 8601
+      const isoDate = new Date(`${year}-02-${day}T${time}`);
+      // console.log();
+
+      // Obtenir le timestamp en millisecondes
+      const timestamp = isoDate.getTime();
+
+      setArrayTimestamp((prevArray) => [...prevArray, timestamp]);
+    });
+  };
+
+  useEffect(() => {
+    getTimestamp();
+  }, [reservHistory]);
+
+  console.log(arrayTimestamp);
 
   return (
     <>
